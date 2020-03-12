65d615dd65fcb7036f5a0f25826fef1171a2807a
DN <1520762935@qq.com>
2020-03-12 18:51:46 +0800

    build exe

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import urllib
 import requests
 import re
 import time
+import os
+import ctypes
 def get_image():
     url = 'http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1361089515117&FORM=HYLH1'
     local = time.strftime("%Y.%m.%d")
@@ -10,22 +12,25 @@ def get_image():
     text = re.findall(reg, content)  
     img_url = 'http://cn.bing.com' + text[0]
     image = requests.get(img_url).content
-    file_name = r'C:\Users\ND\WallPaper\%s.bmp'%local
+    file_name = r'C:\Users\Public\Pictures\WallPaper\%s.bmp'%local
     f = open(file_name, 'wb')
     f.write(image)
     f.close()
     return file_name
 
-import os
-import ctypes
-
 def Set_WP(file_name):
     WP=file_name
     ctypes.windll.user32.SystemParametersInfoW(20, 0, WP, 0)
+ 
 
 def main():
-    WP=get_image()
-    Set_WP(WP)
+    if os.path.exists(r'C:\Users\Public\Pictures\WallPaper'):
+        WP=get_image()
+        Set_WP(WP)
+    else:
+        os.mkdir(r'C:\Users\Public\Pictures\WallPaper')
+        WP=get_image()
+        Set_WP(WP)
 
 if __name__=='__main__':
     main()
